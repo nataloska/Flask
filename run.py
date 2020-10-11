3b9ad0668bd2b1fe0ddd106ff467a2ff4cf072fc
nataloska <natalia.drnkova@gmail.com>
2020-10-11 16:26:21 +0000

    json start

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import os
+import json
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -11,17 +13,21 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title
+        ="About", list_of_numbers=[1, 2, 3], company = data)
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":
