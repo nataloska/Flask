4119f54043af78e3dcd376b71faece7193651b61
nataloska <natalia.drnkova@gmail.com>
2020-10-17 21:32:13 +0000

    contact

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -17,11 +17,25 @@ def about():
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title
-        ="About", list_of_numbers=[1, 2, 3], company = data)
+        ="About", company = data)
 
 
-@app.route("/contact")
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
+
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        print(request.form["name"])
     return render_template("contact.html", page_title="Contact")
 
 
